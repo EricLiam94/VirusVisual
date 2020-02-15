260633547bc653c60a5d429e5726ff174f4178af
EricLiam94 <ericliamhjx@gmail.com>
2020-02-16 00:54:20 +1100

    bug fixed

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -19,10 +19,36 @@ const Chart = ({ type }) => {
     "source-layer": "landuse",
     filter: ["==", "class", "water"]
   };
+  const fetchData = async () => {
+    var geoCord = require("./geo");
+    var url =
+      "https://gitcdn.xyz/cdn/BlankerL/DXY-COVID-19-Data/cb9e825d0726eb3bc618eb8265d5ad9eeeb2c9fd/json/DXYArea.json";
+
+    const data = await fetch(url);
+    const res = await data.json();
+    var temp = res.results.filter(item => item.country === "中国");
+    var output = temp.map(
+      ({
+        provinceShortName,
+        confirmedCount,
+        suspectedCount,
+        curedCount,
+        deadCount
+      }) => ({
+        name: provinceShortName,
+        longitude: geoCord[provinceShortName][0],
+        latitude: geoCord[provinceShortName][1],
+        confirmedCount,
+        suspectedCount,
+        curedCount,
+        deadCount
+      })
+    );
+    setData(output);
+  };
 
   useEffect(() => {
-    var func = require("./test");
-    func(setData);
+    fetchData();
   }, []);
 
   const APK =
